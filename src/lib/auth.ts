b1014306202f5e37116d4621b6a1ed74ccc5a76a
Ahmeddraz-e <ahmeddraz791@gmail.com>
2025-12-15 21:08:24 +0200

    Fix: Update Next.js to latest secure version4

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,7 @@ import { decode } from "next-auth/jwt";
 
 export async function getUserToken() {
     const decodedToken = (await cookies()).get("next-auth.session-token")?.value
-    const token = await decode({ token: decodedToken, secret: process.env.AUTH_SECRET! })
-    
+    const token = await decode({ token: decodedToken, secret: process.env.NEXTAUTH_SECRET! })
+
     return token?.token
 }
